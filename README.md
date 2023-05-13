269d3bb6db9b0595aea1ed98cc53b479d5b05a7d
Nuhddy <noahpc@live.dk>
2023-05-13 02:35:17 +0200

    added filelists, added arg parsing, small refactor

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Lists script files found in both your Witcher 3 mods folder and in a text file containing a list of file paths. Useful for checking which mods from your current modlist should be updated to work on a specific version of the game.
 
-Only works on Linux and WSL for now.
+The included filelists cover scripts from every game update since v4.01.
+
+Only works on Linux and WSL for now (path format issue).
+
+Thanks to [ElementaryLewis](https://github.com/ElementaryLewis) for compiling the update changelogs [here](https://github.com/ElementaryLewis/Witcher-3-Next-Gen-Changelog).
--- w3-modlist-changed-files.py
+++ w3-modlist-changed-files.py
@@ -1,30 +1,80 @@
+import argparse
 import os
 
-GAME_PATH = "/mnt/c/Games/GOG/The Witcher 3 Wild Hunt GOTY/mods"
+LATEST_FILELIST = "./filelists/4_03.txt"
 
-if __name__ == "__main__":
-    mod_list = os.listdir(GAME_PATH)
-    mod_list.remove("mod0000_MergedFiles")
 
-    with open("files.txt") as f:
-        updated_scripts = f.read().splitlines()
+def parse_args():
+    p = argparse.ArgumentParser()
 
-    for mod in mod_list:
-        script_list = []
-        script_path = os.path.join(GAME_PATH, mod, "content")
-        for root, dirs, files in os.walk(script_path):
+    p.add_argument("-p", "--path", required=True)
+    p.add_argument("-f", "--filelist")
+
+    args = p.parse_args()
+    return args
+
+
+def get_intersect(modlist, filelist, mods_path):
+    intersect_mods = {}
+
+    for mod in modlist:
+        local_scripts = []
+        local_script_path = os.path.join(mods_path, mod, "content")
+        for root, dirs, files in os.walk(local_script_path):
             for file in files:
-                script_list.append(os.path.relpath(os.path.join(root, file), start=script_path))
+                local_scripts.append(os.path.relpath(os.path.join(root, file), start=local_script_path))
 
-        changed_scripts = []
-        for file in set(script_list).intersection(updated_scripts):
-            changed_scripts.append(file)
+        intersect_scripts = []
+        for file in set(local_scripts).intersection(filelist):
+            intersect_scripts.append(file)
 
-        if len(changed_scripts) == 0:
+        if len(intersect_scripts) == 0:
             continue
 
-        print("# " + mod)
-        changed_scripts.sort()
-        for file in changed_scripts:
+        intersect_scripts.sort()
+        intersect_mods[mod] = intersect_scripts
+
+    return intersect_mods
+
+
+def print_intersect(mod_dict):
+    for mod in mod_dict:
+        print("### " + mod + "\n")
+        for file in mod_dict[mod]:
             print("- " + file)
         print()
+
+
+def main():
+    args = parse_args()
+
+    if os.path.exists(args.path):
+        mods_path = args.path
+    else:
+        exit(1)
+
+    if args.filelist:
+        if not os.path.exists(args.filelist):
+            exit(1)
+        filelist_path = args.filelist
+    elif os.path.exists(LATEST_FILELIST):
+        filelist_path = LATEST_FILELIST
+    else:
+        exit(1)
+
+    # Get modlist
+    modlist = os.listdir(mods_path)
+    if "mod0000_MergedFiles" in modlist:
+        modlist.remove("mod0000_MergedFiles")
+
+    # Get filelist
+    with open(filelist_path) as f:
+        filelist = f.read().splitlines()
+
+    # Find files present in both modlist and filelist
+    intersect_mods = get_intersect(modlist, filelist, mods_path)
+    print_intersect(intersect_mods)
+
+
+if __name__ == "__main__":
+    main()
